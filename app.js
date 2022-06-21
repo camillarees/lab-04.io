e406ea9e4ab37def11b669defb81c4729563b43a
Camilla Rees <camilla.c.rees@gmail.com>
2022-06-21 11:28:11 -0600

    update just in case

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 
 let username = prompt("Create a Username")
-document.write("Hello " + username "!")
-if (username == "") {
+document.write("Hello " + username)
+if (username === "") {
 error = ("No value was entered. Please try again.")
 alert(error);
 return false;
@@ -13,14 +13,14 @@ if(whatColor === "blue")
 document.write(whatColor + " is my favorite too!");
 else
 document.write("That's a nice color");
-}
 
 getFavColor();
+}
 
 function getAnswer (){
 let moodboardColor = prompt("What color do you want to see on this moodboard?");
 if(moodboardColor === "")
 document.write(moodboardColor + " is a great color! Thanks for your input.");
-}
     
 getAnswer();
+}

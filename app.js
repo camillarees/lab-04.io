202a03b430095cf6419be1ec3cb8d8832089b5ab
Camilla Rees <camilla.c.rees@gmail.com>
2022-06-22 20:46:17 -0600

    added loops

--- app.js
+++ app.js
@@ -1,25 +1,41 @@
 
-let username = prompt("Create a Username")
-document.write("Hello " + username)
-if (username === "") {
-alert("No value was entered. Please try again.")
-return false;
+function createUsername (){   
+    let username = prompt("Create a Username")
+    document.write("Hello " + username)
+    if (username === "") {
+    alert("No value was entered. Please try again.")
+    return false;
+    }
 }
+
+createUsername();
 
 function getFavColor (){
-let whatColor = prompt("What's your favorite color?");
-if(whatColor === "blue")
-document.write(whatColor + " is my favorite too!");
-else
-document.write("That's a nice color");
+    let color = "blue";
+    while (whatColor !== color) {
+        let whatColor = prompt("What's your favorite color?").toLowerCase();
+    }
+    document.write(whatColor + " is my favorite too!");
+
+}
 
 getFavColor();
-}
 
 function getAnswer (){
-let moodboardColor = prompt("What color do you want to see on this moodboard?");
-if(moodboardColor === "")
-document.write(moodboardColor + " is a great color! Thanks for your input.");
+    let moodboardColor = prompt("What color do you want to see on this moodboard?").toLowerCase();
+    document.write(moodboardColor + " is a great color! Thanks for your input.");
     
-getAnswer();
 }
+
+getAnswer();
+
+function sunsetPictures() {
+    let pictures = prompt("How many sunset pictures do you want?");
+    for (let i = 1; i <= pictures; i++) {
+        document.write(<p><img src="./img_sunset.jpg" alt="sunset"></p>)
+    }
+    
+}
+
+sunsetPictures();
+
